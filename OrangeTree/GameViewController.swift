b62534301e2caf71a1ee21c495fe9b046e82d13d
2mvang <marissa.vang@students.dominican.edu>
2020-01-30 15:43:00 -0800

    progress! orange moving!

--- OrangeTree/GameViewController.swift
+++ OrangeTree/GameViewController.swift
@@ -14,19 +14,15 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = GameScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
-                
                 // Present the scene
                 view.presentScene(scene)
             }
-            
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
         }
